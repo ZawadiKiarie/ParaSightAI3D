c0f531edd41e1ec3f10c9f47f57b930c9ee18319
Zawadi Kiarie <zawadikiarie@gmail.com>
2026-05-12 14:25:56 +0300

    add sample prep ui

--- src/components/LabExperience.jsx
+++ src/components/LabExperience.jsx
@@ -4,10 +4,23 @@ import { Model8 } from "./LabModel8";
 import { memo } from "react";
 
 export const LabExperience = memo(
-  ({ moveInput, lookInput, onPlayerMovedEnough, onStationClick }) => {
+  ({
+    moveInput,
+    lookInput,
+    onPlayerMovedEnough,
+    onStationClick,
+
+    samplePrepStep,
+    samplePrepCameraActive,
+    samplePrepCompleted,
+    onStartSamplePrep,
+    onAddSample,
+    onApplyStain,
+  }) => {
     return (
       <>
         <Environment preset="city" />
+
         <Model8
           position={[-35, -5, -5]}
           scale={3}
@@ -15,6 +28,12 @@ export const LabExperience = memo(
           lookInput={lookInput}
           onPlayerMovedEnough={onPlayerMovedEnough}
           onStationClick={onStationClick}
+          samplePrepStep={samplePrepStep}
+          samplePrepCameraActive={samplePrepCameraActive}
+          samplePrepCompleted={samplePrepCompleted}
+          onStartSamplePrep={onStartSamplePrep}
+          onAddSample={onAddSample}
+          onApplyStain={onApplyStain}
         />
       </>
     );
