ed6b247174fef88fe06dfc7a1cc6df58e4c1d3c7
Zawadi Kiarie <zawadikiarie@gmail.com>
2026-05-17 18:51:34 +0300

    update spelling

--- src/components/guidedlearning/IsolatedFeatureOvelay.jsx
+++ src/components/guidedlearning/IsolatedFeatureOvelay.jsx
@@ -8,7 +8,7 @@ import {
   focusedMarkerIdAtom,
   hoveredMarkerAtom,
   infoSectionAtom,
-} from "../../store/store";
+} from "../../store/Store";
 import { PARASITE_DATA, getAvailableStages } from "../ParasiteConfig";
 import { INFO_SECTIONS, getFeatureContent } from "../../store/FeatureContent";
 
